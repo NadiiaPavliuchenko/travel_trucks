35b5b0e397b95bb2fe05be6240c2e5e43a701e04
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-11-20 17:27:07 +0200

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+    },
   },
+  base: "/travel_trucks/",
 });
