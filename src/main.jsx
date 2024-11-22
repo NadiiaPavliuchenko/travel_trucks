4110c9ea8bb90dfa3beb4f00a21d1e314d3bda4f
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-11-22 16:30:35 +0200

    fix for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/travel_trucks">
+    <BrowserRouter>
       <RootStyles />
       <App />
     </BrowserRouter>
